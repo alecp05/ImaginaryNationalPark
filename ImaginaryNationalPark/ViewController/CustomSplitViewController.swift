c7867907cc5594b5751c5e99d31b7bf7df44f7e1
Alec <alexander.pureza@bitsfabrik.com>
2023-09-13 13:19:42 +0200

    ✨ CustomSplitViewController and configured

---
+++ ImaginaryNationalPark/ViewController/CustomSplitViewController.swift
@@ -0,0 +1,26 @@
+//
+//  CustomSplitViewController.swift
+//  ImaginaryNationalPark
+//
+//  Created by Alec on 13.09.23.
+//
+
+import UIKit
+
+// /////////////////////////////////////////////////////////////////////////
+// MARK: - CustomSplitViewController -
+// /////////////////////////////////////////////////////////////////////////
+
+class CustomSplitViewController: UISplitViewController, UISplitViewControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.delegate = self
+        self.preferredDisplayMode = .oneBesideSecondary
+    }
+
+    func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController: UIViewController, onto primaryViewController: UIViewController) -> Bool {
+        return true
+    }
+}
