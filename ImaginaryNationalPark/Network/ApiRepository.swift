56e685554880370cb9a4dfb2cacb82a48b8f75a1
Alec <alexander.pureza@bitsfabrik.com>
2023-09-18 13:22:02 +0200

    🚚 moving apiRepository to Network

---
+++ ImaginaryNationalPark/Network/ApiRepository.swift
@@ -0,0 +1,79 @@
+//
+//  ApiRepository.swift
+//  ImaginaryNationalPark
+//
+//  Created by Alec on 11.09.23.
+//
+
+import Alamofire
+import Foundation
+
+// /////////////////////////////////////////////////////////////////////////
+// MARK: - ApiRepository -
+// /////////////////////////////////////////////////////////////////////////
+
+class ApiRepository {
+    
+    // /////////////////////////////////////////////////////////////////////////
+    // MARK: - Properties
+    
+    var allTours: [Tour] = [Tour]()
+    var detailTour: Tour?
+    
+    private let headers: HTTPHeaders = [.accept("application/json")]
+    
+    // /////////////////////////////////////////////////////////////////////////
+    // MARK: - Functions
+    
+    func getTours(type: Request, completed: @escaping () -> Void) {
+        
+        AF.request(type.value, headers: self.headers)
+            .responseDecodable(of: [Tour].self) { response in
+                
+                switch response.result {
+                case .success(let tours):
+                    
+                    if !self.allTours.isEmpty {
+                        self.allTours.removeAll()
+                    }
+                    
+                    for tour in tours {
+                        self.allTours.append(tour)
+                    }
+                    
+                    completed()
+                case .failure(let error):
+                    print(error)
+                }
+            }
+    }
+    
+    func getTourWithID(id: Int, completed: @escaping (Tour) -> Void) {
+        
+        AF.request(Request.tourDetail(id).value, headers: self.headers)
+            .responseDecodable(of: Tour.self) { response in
+                
+                switch response.result {
+                case .success(let tour):
+                
+                    self.detailTour = tour
+                    completed(tour)
+                case .failure(let error):
+                    print(error)
+                }
+            }
+    }
+    
+    func getContactInfo(completed: @escaping (Contact) -> Void) {
+        AF.request(Request.contact.value, headers: self.headers)
+            .responseDecodable(of: Contact.self) { response in
+                
+                switch response.result {
+                case .success(let contact):
+                    completed(contact)
+                case .failure(let error):
+                    print(error)
+                }
+            }
+    }
+}
