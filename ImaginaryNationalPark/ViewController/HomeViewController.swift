fec6e62092035a4a25e53155bfede00522d0aa64
Alec <alexander.pureza@bitsfabrik.com>
2023-09-18 09:31:31 +0200

    ♻️ delegate and datasource refactoring

--- ImaginaryNationalPark/ViewController/HomeViewController.swift
+++ ImaginaryNationalPark/ViewController/HomeViewController.swift
@@ -12,7 +12,7 @@ import UIKit
 // MARK: - HomeViewController Delegate -
 // /////////////////////////////////////////////////////////////////////////
 
-protocol HomeViewControllerDelegate {
+protocol HomeViewControllerDelegate: NSObjectProtocol {
     func didSelectTour(tour: Tour)
 }
 
@@ -60,13 +60,12 @@ class HomeViewController: UIViewController, UITableViewDelegate {
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.sectionHeaderTopPadding = 0
         tableView.estimatedRowHeight = 120
-        tableView.rowHeight = UITableView.automaticDimension
         tableView.register(TourCell.self, forCellReuseIdentifier: TourCell.reuseIdentifier)
         tableView.delegate = self
         return tableView
     }()
     
-    private var dataSource: TourDataSource?
+    private lazy var dataSource: TourDataSource = self.configureDataSource()
     
     private var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
@@ -76,7 +75,7 @@ class HomeViewController: UIViewController, UITableViewDelegate {
     
     private let repository: ApiRepository = ApiRepository()
     
-    var delegate: HomeViewControllerDelegate! = nil
+    weak var delegate: HomeViewControllerDelegate?
     
     // /////////////////////////////////////////////////////////////////////////
     // MARK: - HomeViewController
@@ -92,8 +91,7 @@ class HomeViewController: UIViewController, UITableViewDelegate {
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: self.imageLogo)
         
         // tableView
-        self.configureDataSource()
-        self.dataSource?.update(requestType: .allTours, repository: self.repository)
+        self.dataSource.update(requestType: .allTours, repository: self.repository)
         
         // subviews
         self.view.addSubview(self.allTourButton)
@@ -127,7 +125,7 @@ class HomeViewController: UIViewController, UITableViewDelegate {
             make.top.equalTo(self.allTourButton.snp.bottom)
             make.trailing.equalTo(self.view.safeAreaLayoutGuide.snp.trailing)
             make.leading.equalTo(self.view.safeAreaLayoutGuide.snp.leading)
-            make.bottom.equalToSuperview()
+            make.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom)
         }
     }
     
@@ -136,13 +134,13 @@ class HomeViewController: UIViewController, UITableViewDelegate {
     
     @objc
     func clickedAllTourButton() {
-        self.dataSource?.update(requestType: .allTours, repository: self.repository)
+        self.dataSource.update(requestType: .allTours, repository: self.repository)
         self.tableView.reloadData()
     }
     
     @objc
     func clickedTopFiveButton() {
-        self.dataSource?.update(requestType: .topFive, repository: self.repository)
+        self.dataSource.update(requestType: .topFive, repository: self.repository)
         self.tableView.reloadData()
     }
     
@@ -150,8 +148,8 @@ class HomeViewController: UIViewController, UITableViewDelegate {
     // MARK: - DiffableDataSource
     // /////////////////////////////////////////////////////////////////////////
     
-    func configureDataSource() {
-        self.dataSource = TourDataSource(tableView: self.tableView) { tableView, indexPath, tour -> UITableViewCell in
+    func configureDataSource() -> TourDataSource {
+        return TourDataSource(tableView: self.tableView) { tableView, indexPath, tour -> UITableViewCell in
             
             guard let cell = tableView.dequeueReusableCell(withIdentifier: TourCell.reuseIdentifier, for: indexPath) as? TourCell
             else { fatalError("Couldn't create TourCell") }
@@ -187,12 +185,12 @@ class HomeViewController: UIViewController, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        if let tour = self.dataSource?.itemIdentifier(for: indexPath) {
+        if let tour = self.dataSource.itemIdentifier(for: indexPath) {
             
             self.repository.getTourWithID(id: tour.id, completed: { tour in
                 
                 if UIScreen.main.bounds.width > UIScreen.main.bounds.height || UIDevice.current.userInterfaceIdiom == .pad {
-                    self.delegate.didSelectTour(tour: tour)
+                    self.delegate?.didSelectTour(tour: tour)
                 } else {
                     let controller = DetailViewController(tour: tour, repository: self.repository)
                     self.navigationController?.pushViewController(controller, animated: true)
