21390648962bd1c2df7802d06d80bb175b3ff18e
Alec <alexander.pureza@bitsfabrik.com>
2023-09-20 15:21:13 +0200

    ✨ added HomeViewModel

---
+++ ImaginaryNationalPark/ViewModel/HomeViewModel.swift
@@ -0,0 +1,43 @@
+//
+//  HomeViewModel.swift
+//  ImaginaryNationalPark
+//
+//  Created by Alec on 20.09.23.
+//
+
+import Foundation
+import ViewModel
+import RxSwift
+import RxCocoa
+
+// /////////////////////////////////////////////////////////////////////////
+// MARK: - HomeViewModel -
+// /////////////////////////////////////////////////////////////////////////
+
+class HomeViewModel: ViewModel {
+    
+    // /////////////////////////////////////////////////////////////////////////
+    // MARK: - Properties
+    
+    private let repository: ApiRepository = ApiRepository()
+    let tours: BehaviorRelay<[Tour]> = BehaviorRelay<[Tour]>(value: [])
+    
+    // /////////////////////////////////////////////////////////////////////////
+    // MARK: - Life Cycle
+    
+    override init() {
+        super.init()
+        
+        self.getTours(service: Service.allTours)
+    }
+    
+    // /////////////////////////////////////////////////////////////////////////
+    // MARK: - Functions
+    
+    func getTours(service: Service) {
+        self.repository.getTours(service: service, completed: { tours in
+            
+            self.tours.accept(tours)
+        })
+    }
+}
